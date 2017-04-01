cf480268c6bfe0da22970817e0f4422cfee995fc
Рогов Александр <rogov@info74.ru>
2017-04-01 17:10:56 +0500

    implemented text input

--- app/src/main/java/ru/rogovalex/translator/TranslateFragment.java
+++ app/src/main/java/ru/rogovalex/translator/TranslateFragment.java
@@ -5,13 +5,26 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 public class TranslateFragment extends Fragment {
+
+    EditText mTextInput;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_translate, container, false);
+        View view = inflater.inflate(R.layout.fragment_translate, container, false);
+
+        mTextInput = (EditText) view.findViewById(R.id.text_input);
+        view.findViewById(R.id.clear).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTextInput.setText("");
+            }
+        });
+
+        return view;
     }
 
 }
